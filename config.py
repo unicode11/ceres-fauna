a52de9d0f37712c0e0798caa1a846d21e4ccc934
unicode1 <egoraleshin2006@gmail.com>
2025-06-20 01:46:10 +0300

    конфиг 5

--- config.py
+++ config.py
@@ -15,10 +15,33 @@ def make_path(
         return filepath
     
 def read(path:str):
-    with open(path, "r") as f:
-        return json.load(f)
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+    except:
+        "couldn't read"
+    
+def populate_root():
+    with open(ROOT_PATH,'r', encoding="utf-8") as f:
+        data = json.load(f)
+        
+    penis = {
+        "token": "BOT_TOKEN_HERE",
+        "root_id": "ADMIN_DISCORD_ID"
+    }
+    
+    required = {"token","root_id"}
+    if not required.issubset(data):
+        with open(ROOT_PATH, 'w', encoding='utf-8') as f:
+            print(f"saving {penis}")
+            json.dump(penis, f, ensure_ascii=False, indent=4)
+        
+# no edit above
 
 USERS_PATH = make_path("users.json", True)  # path that will be created by "add_user" command
 ROOT_PATH = make_path("root_data.json", True)  # path that have bot token and root user discord ID
 XRAY_CFG_PATH = make_path("config_xray.json", False)  # path, created by xray service, no need to create this by bot
 
+# no edit below
+
+populate_root()
