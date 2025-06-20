f2bb8274d9de8c8c03dc10f0a37e1c21f54f93a7
unicode1 <egoraleshin2006@gmail.com>
2025-06-20 17:06:12 +0300

    идеи 13

--- payment.py
+++ payment.py
@@ -12,6 +12,7 @@ from config import *
 YM = read(ROOT_PATH)["yoomoney_id"]
 YM_URL = "https://yoomoney.ru/quickpay/confirm"
 WB = "https://discord.com/api/webhooks/1385566546155274360/k3sdztVXjfxDuswWMowi1M0iVEzQInbzFFNkgafHAPdQibGcCTigO8pcuR30BLqn8cUM"
+GIF = "https://cdn.discordapp.com/attachments/1149826600167297135/1226869385323483136/14.gif?ex=6856a3a7&is=68555227&hm=2f960382d560aa7e09dc7f8350f17841d0b53457cc20cd041e362c2c651c7030&"
 
 def send(data: dict):
     """
@@ -111,7 +112,7 @@ class payment(commands.Cog):
         
         # with open("payments.json", "a", encoding="utf-8") as f:
         #     f.write(json.dumps(penis) + "\n")
-    
+        await interaction.user.send(GIF)
         await interaction.response.send_message(
             f"К оплате: **{amount}₽**, {payment_url}\n~~Уникальный label для этой оплаты - `{label}` (сохраните его для проверки оплаты)~~ <- Забей хуй, пока в разработке /shrug",
             ephemeral=True
